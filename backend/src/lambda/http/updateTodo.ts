99b4549ad3b77e6d23dc7267d48e77fa7363b16e
Diallo <105805595+oury011@users.noreply.github.com>
2022-10-17 01:20:51 +0000

    feat: serverless app v1

---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,38 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+import { updateTodo } from '../../businessLogic/todos'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+    // DONE: Update a TODO item with the provided id using values in the "updatedTodo" object
+    const userId = getUserId(event)
+    await updateTodo(
+      todoId,
+      userId,
+      updatedTodo,
+    )
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
